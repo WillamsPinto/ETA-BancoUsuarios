3c47e5f8a8bd83ad962f9d4bff589f72b240b878
WillamsPinto <willamspinto18@gmail.com>
2023-02-01 22:21:09 -0300

    Implementação dos métodos de alterar, buscar e remover usuario

--- src/service/service_usuario.py
+++ src/service/service_usuario.py
@@ -6,6 +6,18 @@ class ServiceUsuario:
     def __init__(self):
         self.store = Store()
 
+    def verificar_usuario(self, nome, profissao):
+        for usuario in self.store.bd:
+            if usuario.nome == nome and usuario.profissao == profissao:
+                return True
+        return False
+
+    def buscar_usuario(self, nome, profissao):
+        for usuario in self.store.bd:
+            if usuario.nome == nome and usuario.profissao == profissao:
+                return usuario
+        return "Usuario não encontrado"
+
     def add_usuario(self, nome, profissao):
         if nome != None and profissao != None:
             if type(nome) == str and type(profissao) == str:
@@ -17,27 +29,23 @@ class ServiceUsuario:
         else:
             return "Usuario invalido"
 
-    def buscar_usuario(self, nome, profissao):
-        for usuario in self.store.bd:
-            if usuario.nome == nome and usuario.profissao == profissao:
-                return True
-
-        return False
-
-
+    def change_usuario(self, nome, profissao, newName, newProfissao):
+        if type(nome) == str and type(profissao) == str and nome != None and profissao != None and newName != None and newProfissao != None:
+            if self.verificar_usuario(nome, profissao):
+                usuario = Usuario(newName, newProfissao)
+                self.store.bd[self.store.bd.index(self.buscar_usuario(nome, profissao))] = usuario
+                return "Usuario atualizado"
+            else:
+                return "Usuario não atualizado"
+        else:
+            return "Usuario não atualizado"
 
     def delete_usuario(self, nome, profissao):
-
-        if nome != None and profissao != None:
-            if type(nome) == str and type(profissao) == str:
-                if self.buscar_usuario(nome, profissao):
-                    usuario = Usuario(nome, profissao)
-                    self.store.bd.remove(usuario)
-                    return "Usuario removido"
-                else:
-                    return "Usuario nao removido"
-
+        if type(nome) == str and type(profissao) == str and nome != None and profissao != None:
+            if self.verificar_usuario(nome, profissao):
+                self.store.bd.remove(self.buscar_usuario(nome, profissao))
+                return "Usuario removido"
             else:
-                return "Usuario nao removido"
+                return "Usuario não removido"
         else:
-            return "Usuario nao removido"
+            return "Usuario não removido"
--- test/service/test_service_usuario.py
+++ test/service/test_service_usuario.py
@@ -3,6 +3,7 @@ from src.service.service_usuario import ServiceUsuario
 
 class TestServiceUsuario:
 
+    #Adicionar Usuario
     def test_add_usuario_nome_valido_profissao_valido(self): #nome e profissão válidos
         #setup
         service = ServiceUsuario()
@@ -63,3 +64,198 @@ class TestServiceUsuario:
         assert result == result_esperado
         assert service.store.bd == store_esperado
 
+    #Buscar Usuario
+    def test_buscar_usuario_nome_valido_profissao_valida(self):
+        #Setup
+        service = ServiceUsuario()
+        nome_valido = "Willams"
+        profissao_valida = "Eng"
+        service.add_usuario(nome_valido, profissao_valida)
+
+        #chamada
+        result = service.buscar_usuario(nome_valido, profissao_valida)
+
+        #Avaliação
+        assert result.nome == nome_valido
+        assert result.profissao == profissao_valida
+
+    def test_buscar_usuario_nome_invalido_profissao_valida(self):
+        #Setup
+        service = ServiceUsuario()
+        nome_invalido = None
+        profissao_valida = "Eng"
+        service.add_usuario("Willams", profissao_valida)
+        result_esperado = "Usuario não encontrado"
+
+        #chamada
+        result = service.buscar_usuario(nome_invalido, profissao_valida)
+
+        #Avaliação
+        assert result == result_esperado
+
+    def test_buscar_usuario_nome_valido_profissao_invalida(self):
+        #Setup
+        service = ServiceUsuario()
+        nome_valido = "Willams"
+        profissao_invalida = None
+        service.add_usuario(nome_valido, profissao_invalida)
+        result_esperado = "Usuario não encontrado"
+
+        #chamada
+        result = service.buscar_usuario(nome_valido, profissao_invalida)
+
+        #Avaliação
+        assert result == result_esperado
+
+    def test_buscar_usuario_nome_invalido_profissao_invalida(self):
+        #Setup
+        service = ServiceUsuario()
+        nome_invalido = None
+        profissao_invalida = None
+        service.add_usuario("Willams", "Eng")
+        result_esperado = "Usuario não encontrado"
+
+        #chamada
+        result = service.buscar_usuario(nome_invalido, profissao_invalida)
+
+        #Avaliação
+        assert result == result_esperado
+
+
+    #Alterar Usuario
+    def test_alterar_usuario_newnome_valido_newprofissao_valida(self):
+        # setup
+        service = ServiceUsuario()
+        nome_valido = "Willams"
+        profissao_valida = "Eng"
+        newNome_valido = "Talita"
+        newProfissao_valido = "Dev"
+        service.add_usuario(nome_valido, profissao_valida)
+        result_esperado = "Usuario atualizado"
+
+        # chamada
+        result = service.change_usuario(nome_valido, profissao_valida, newNome_valido, newProfissao_valido)
+
+        # avaliação
+        assert result == result_esperado
+        assert service.store.bd[0].nome == newNome_valido
+        assert service.store.bd[0].profissao == newProfissao_valido
+
+    def test_alterar_usuario_newnome_invalido_newprofissao_valida(self):
+        # setup
+        service = ServiceUsuario()
+        nome_valido = "Willams"
+        profissao_valida = "Eng"
+        newNome_invalido = None
+        newProfissao_valido = "Dev"
+        service.add_usuario(nome_valido, profissao_valida)
+        result_esperado = "Usuario não atualizado"
+
+        # chamada
+        result = service.change_usuario(nome_valido, profissao_valida, newNome_invalido, newProfissao_valido)
+
+        # avaliação
+        assert result == result_esperado
+        assert service.store.bd[0].nome == nome_valido
+        assert service.store.bd[0].profissao == profissao_valida
+
+    def test_alterar_usuario_newnome_valido_newprofissao_invalida(self):
+        # setup
+        service = ServiceUsuario()
+        nome_valido = "Willams"
+        profissao_valida = "Eng"
+        newNome_valido = "Talita"
+        newProfissao_invalido = None
+        service.add_usuario(nome_valido, profissao_valida)
+        result_esperado = "Usuario não atualizado"
+
+        # chamada
+        result = service.change_usuario(nome_valido, profissao_valida, newNome_valido, newProfissao_invalido)
+
+        # avaliação
+        assert result == result_esperado
+        assert service.store.bd[0].nome == nome_valido
+        assert service.store.bd[0].profissao == profissao_valida
+
+    def test_alterar_usuario_newnome_invalido_newprofissao_invalida(self):
+        # setup
+        service = ServiceUsuario()
+        nome_valido = "Willams"
+        profissao_valida = "Eng"
+        newNome_invalido = None
+        newProfissao_invalido = None
+        service.add_usuario(nome_valido, profissao_valida)
+        result_esperado = "Usuario não atualizado"
+
+        # chamada
+        result = service.change_usuario(nome_valido, profissao_valida, newNome_invalido, newProfissao_invalido)
+
+        # avaliação
+        assert result == result_esperado
+        assert service.store.bd[0].nome == nome_valido
+        assert service.store.bd[0].profissao == profissao_valida
+
+    #Remover Usuario
+    def test_remover_usuario_nome_valido_profissao_valida(self):
+        # setup
+        service = ServiceUsuario()
+        nome_valido = "Willams"
+        profissao_valida = "Eng"
+        service.add_usuario(nome_valido, profissao_valida)
+        result_esperado = "Usuario removido"
+        store_esperado = []
+
+        # chamada
+        result = service.delete_usuario(nome_valido, profissao_valida)
+
+        # avaliação
+        assert result == result_esperado
+        assert service.store.bd == store_esperado
+
+    def test_remover_usuario_nome_invalido_profissao_valida(self):
+        # setup
+        service = ServiceUsuario()
+        nome_invalido = None
+        profissao_valida = "Eng"
+        service.add_usuario("Willams", profissao_valida)
+        result_esperado = "Usuario não removido"
+        store_empty = []
+
+        # chamada
+        result = service.delete_usuario(nome_invalido, profissao_valida)
+
+        # avaliação
+        assert result == result_esperado
+        assert service.store.bd != store_empty
+
+    def test_remover_usuario_nome_valido_profissao_invalida(self):
+        # setup
+        service = ServiceUsuario()
+        nome_valido = "Willams"
+        profissao_invalida = None
+        service.add_usuario(nome_valido, "Eng")
+        result_esperado = "Usuario não removido"
+        store_empty = []
+
+        # chamada
+        result = service.delete_usuario(nome_valido, profissao_invalida)
+
+        # avaliação
+        assert result == result_esperado
+        assert service.store.bd != store_empty
+
+    def test_remover_usuario_nome_invalido_profissao_invalida(self):
+        # setup
+        service = ServiceUsuario()
+        nome_invalido = None
+        profissao_invalida = None
+        service.add_usuario("Willams", "Eng")
+        result_esperado = "Usuario não removido"
+        store_empty = []
+
+        # chamada
+        result = service.delete_usuario(nome_invalido, profissao_invalida)
+
+        # avaliação
+        assert result == result_esperado
+        assert service.store.bd != store_empty
